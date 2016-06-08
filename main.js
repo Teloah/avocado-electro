68e612c593afd44a1cc8f3b5555b20357287b5e7
Andris Klaipiņš <teloah@gmail.com>
2016-06-09 00:01:48 +0300

    Marking selected section

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 var fs = require("fs");
 
 $('.nav-btn').on('click', function() {
+	$(".nav-btn").removeClass("selected");
 	var source = fs.readFileSync(__dirname + '/pages/' + this.id + '.html', "utf-8");
 	document.getElementById("main").innerHTML  = source;
+	$(this).addClass("selected");
 });
