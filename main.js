51cb17388004b756a5d4c2ca23ae6ed088a61ddf
Andris Klaipiņš <teloah@gmail.com>
2016-06-16 02:14:36 +0300

    Visual update; adding New buttons

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ function selectPage(id) {
 	$(".nav-btn").removeClass("selected");
 	var source = fs.readFileSync(__dirname + '/pages/' + id + '.html', "utf-8");
 	document.getElementById("main").innerHTML  = source;
-	document.getElementById("top-bar").innerHTML = '<p>' + id + '</p>';
+	document.getElementById("top-bar").innerHTML = '<p>' + id.charAt(0).toUpperCase() + id.slice(1) + '</p>';
 	$("#" + id).addClass("selected");
 }
 
