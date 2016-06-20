9faf0cb6970c8fe83be09bcfa0f4784a6198733f
Andris Klaipiņš <teloah@gmail.com>
2016-06-21 01:34:30 +0300

    Micro jQuery cleanup

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ var fs = require("fs");
 function selectPage(id) {
 	$(".nav-btn").removeClass("selected");
 	var source = fs.readFileSync(__dirname + '/pages/' + id + '.html', "utf-8");
-	document.getElementById("main").innerHTML  = source;
-	document.getElementById("top-bar").innerHTML = '<p>' + id.charAt(0).toUpperCase() + id.slice(1) + '</p>';
+	$("#main").html(source);
+	$("#top-bar").html('<p>' + id.charAt(0).toUpperCase() + id.slice(1) + '</p>');
 	$("#" + id).addClass("selected");
 }
 
