bab3237ba39a3c04098f8dc63479ab10266ec540
Andris Klaipiņš <teloah@gmail.com>
2016-07-09 02:32:34 +0300

    Moved home page related scripts to home.js

--- app/main.js
+++ app/main.js
@@ -1,29 +1,20 @@
-const fs = require("fs");
-const {ipcRenderer} = require('electron');
+const fs = require("fs"),
+	HomePresenter = require('./scripts/home.js');
 
-ipcRenderer.on('reports-loaded', (event, reports) => {
-	console.log(reports);
-	let table_html = '<table>';
-	reports.forEach(report => {
-		table_html += `<tr><td>${report.report}</td><td>${report.company}</td><td>${report.date}</td><td>${report.comment}</td></tr>`;
-	});
-	table_html += '</table>';
-	$("#main").html(table_html);
-});
-
-function loadReports() {
-	ipcRenderer.send('load-reports', '');
-}
+const presenters = new Map();
+presenters.set('home', new HomePresenter());
 
 function selectPage(id) {
+	console.log(`Switching to ${id}`);
 	$(".nav-btn").removeClass("selected");
 	let source = fs.readFileSync(__dirname + '/pages/' + id + '.html', "utf-8");
 	$("#main").html(source);
 	$("#top-bar").html('<p>' + id.charAt(0).toUpperCase() + id.slice(1) + '</p>');
 	$("#" + id).addClass("selected");
-	console.log(`Switching to ${id}`);
-	if (id === 'home') {
-		loadReports();
+
+	let presenter = presenters.get(id);
+	if (presenter) {
+		presenter.show();
 	}
 }
 
@@ -33,5 +24,4 @@ $('.nav-btn').on('click', function () {
 
 $(document).ready(() => {
 	selectPage("home");
-	loadReports();
 });
---
+++ app/scripts/home.js
@@ -0,0 +1,23 @@
+const {ipcRenderer} = require('electron');
+
+ipcRenderer.on('reports-loaded', (event, reports) => {
+	console.log(reports);
+	let table_html = '<table>';
+	reports.forEach(report => {
+		table_html += `<tr><td>${report.report}</td><td>${report.company}</td><td>${report.date}</td><td>${report.comment}</td></tr>`;
+	});
+	table_html += '</table>';
+	$("#main").html(table_html);
+});
+
+function loadReports() {
+	ipcRenderer.send('load-reports', '');
+}
+
+class HomePresenter {
+    show() {
+        loadReports();
+    }
+}
+
+module.exports = HomePresenter;
