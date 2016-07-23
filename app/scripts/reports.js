5d1ea4cc9bdd024eb6a8d5d68f61ba0684d21704
Andris Klaipiņš <teloah@gmail.com>
2016-07-23 23:58:54 +0300

    Cleaning(?) up reports.js

--- app/scripts/reports.js
+++ app/scripts/reports.js
@@ -1,15 +1,23 @@
 class Report {
+    reportsFor(company, all_reports) {
+        let reports = [];
+        all_reports.forEach(report => {
+            if (report.company === company) {
+                reports.push(report);
+            }
+        });
+        return reports;
+    }
     parseEntries(companies, templates, reports) {
         let entries = [];
         companies.forEach(company => {
-            reports.forEach(report => {
-                if (report.company === company) {
-                    templates.forEach(template => {
-                        if (template.name === report.template) {
-                            entries.push({ report: `${report.template}`, company: `${company}`, date: `201607${template.config}`, comment: `` });
-                        }
-                    });
-                }
+            let company_reports = this.reportsFor(company, reports);
+            company_reports.forEach(report => {
+                templates.forEach(template => {
+                    if (template.name === report.template) {
+                        entries.push({ report: `${report.template}`, company: `${company}`, date: `201607${template.config}`, comment: `` });
+                    }
+                });
             });
         });
         return entries;
