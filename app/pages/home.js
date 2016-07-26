36510473817ca0aab5e1c75880cf63039cb009c7
Andris Klaipiņš <teloah@gmail.com>
2016-07-27 02:20:44 +0300

    Reformatted indentation; experimenting with table styling

--- app/pages/home.js
+++ app/pages/home.js
@@ -1,9 +1,9 @@
 const {ipcRenderer} = require('electron');
 
 class HomePresenter {
-	loadReports() {
-		ipcRenderer.send('load-entries', '');
-	}
+    loadReports() {
+        ipcRenderer.send('load-entries', '');
+    }
     show() {
         this.loadReports();
     }
@@ -11,26 +11,30 @@ class HomePresenter {
 module.exports = HomePresenter;
 
 function formatReportsDOM(reports) {
-	let table_html = `<div class="reports">
-						<div><th>Date</th><th>Name</th><th>Company</th><th>Comments</th></thead>
-						<div>`;
-	reports.forEach(report => {
-		table_html += `<div class="report">`;
-		table_html += `<div class="report_date">${report.date}</div>`;
-		table_html += `<div class="report_name">${report.report}</div>`;
-		table_html += `<div class="report_company">${report.company}</div>`;
-		table_html += `<div class="report_comment">${report.comment}</div>`;
-		table_html += `</div>`;
-	});
-	table_html += `</div></div>`;
-	return table_html;
+    let table_html = `<div class="reports">
+        <div class="report">
+        <div class="report_date report_table_header">Date</div>
+        <div class="report_name report_table_header">Name</div>
+        <div class="report_company report_table_header">Company</div>
+        <div class="report_comment report_table_header">Comments</div>
+        </div>`;
+    reports.forEach(report => {
+        table_html += `<div class="report">`;
+        table_html += `<div class="report_date">${report.date}</div>`;
+        table_html += `<div class="report_name">${report.report}</div>`;
+        table_html += `<div class="report_company">${report.company}</div>`;
+        table_html += `<div class="report_comment">${report.comment}</div>`;
+        table_html += `</div>`;
+    });
+    table_html += `</div>`;
+    return table_html;
 }
 
 ipcRenderer.on('entries-loaded', (event, entries) => {
-	if (entries.length === 0) {
-		return;
-	}
-	let entries_html = formatReportsDOM(entries);
-	$("#main").html(entries_html);
+    if (entries.length === 0) {
+        return;
+    }
+    let entries_html = formatReportsDOM(entries);
+    $("#main").html(entries_html);
 });
 
